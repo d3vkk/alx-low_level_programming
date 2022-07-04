bb309cf9ce8970967b6f370680cf747a2237340c
d3vkk <donaldkk22@yahoo.com>
2022-07-04 15:40:20 +0300

    Show all base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all base 10 digits
+ *
+ * Return: 0 if without error
+ */
+int main(void)
+{
+int i = 0;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+
+return (0);
+}
