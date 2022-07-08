15423cfbf70d595a22aeac31b1b1252a763aa659
d3vkk <donaldkk22@yahoo.com>
2022-07-08 16:24:07 +0300

    Fizz buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Fizz buzz test
+ *
+ * Return: 0 if without error
+ */
+int main(void)
+{
+int i;
+
+for (i = 1; i < 100 ; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("Buzz\n");
+
+return (0);
+}
