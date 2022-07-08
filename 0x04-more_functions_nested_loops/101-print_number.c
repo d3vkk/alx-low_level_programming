bc2ca1558b2170504bd56d9d4b6c62f4e6c9498a
d3vkk <donaldkk22@yahoo.com>
2022-07-08 16:44:40 +0300

    Show an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: is integer
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+unsigned int a;
+unsigned int b;
+unsigned int count;
+
+if (n < 0)
+{
+_putchar(45);
+a = n * -1;
+}
+else
+{
+a = n;
+}
+
+b = a;
+count = 1;
+
+while (b > 9)
+{
+b /= 10;
+count *= 10;
+}
+
+for (; count >= 1; count /= 10)
+{
+_putchar(((a / count) % 10) + 48);
+}
+}
